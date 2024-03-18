477ee30a29113ca6e86463efe7784c8745fe186a
Mbala Sadraque <orlandosadraquiorlandinho9@gmail.com>
2024-03-19 00:45:22 +0100

    criar fillable do produtos

--- app/Models/Admin/Produto.php
+++ app/Models/Admin/Produto.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Produto extends Model
 {
     use HasFactory;
+    protected $table = 'produtos';
+
+    /**
+      * The attributes that are mass assignable.
+      *
+      * @var array<int, string>
+      */
+     protected $fillable = [
+         'name',
+         'status',
+         'description',
+     ];
 }
