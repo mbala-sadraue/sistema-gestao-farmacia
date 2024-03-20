7b49b655902ebe5b3bf68905088bba4b65615aec
Mbala Sadraque <orlandosadraquiorlandinho9@gmail.com>
2024-03-20 01:30:11 +0100

    criar rota para itens

--- routes/custom/admin.php
+++ routes/custom/admin.php
@@ -24,6 +24,7 @@ Route::middleware('auth','role:administrador')->prefix('admin')->group(function(
     Route::get("/", [DashboardController::class,'index'])->name('admin.dashboard');
     
     Route::resource('produto',ProdutoController::class);
+    Route::resource('itens',ProdutoController::class);
     Route::resource('fornecedor',FornecedorController::class);
     // Rota do usuario 
     Route::resource('user',UserController::class);
