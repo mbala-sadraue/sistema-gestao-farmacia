a490334430af77cfdaf031e986e49bd231e4cf37
Mbala Sadraque <orlandosadraquiorlandinho9@gmail.com>
2024-03-21 22:08:30 +0100

    alerta com acor certa

--- resources/views/templates/painel/componentes/alert/alert.blade.php
+++ resources/views/templates/painel/componentes/alert/alert.blade.php
@@ -1,19 +1,16 @@
   @if(session('status'))
 
-   @if(session('status')['status'] == true)
-
-    <div class="alert alert-status alert-dismissible fade show" role="alert"
-     id="alert">
-        <i class="bi bi-check-circle-fill me-1"></i>
-         {!! session('status')['messages'] !!}
-        <button type="button" class="btn-close" data-bs-dismiss="alert"
-         aria-label="Close"></button>
-    </div>
+  @if (session('status')['status'] == true)
+      <div class="alert alert-success alert-dismissible fade show" role="alert" id="alert">
+          <i class="bi bi-check-circle-fill me-1"></i>
+          {!! session('status')['messages'] !!}
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
     @elseif(session('status')['status'] == false)
       <div class="alert alert-danger alert-dismissible fade show" role="alert" id="alert">
           <i class="bi bi-x-circle-fill me-1"></i>
-              {!! session('status')['messages'] !!}
+          {!! session('status')['messages'] !!}
           <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-   @endif
+      </div>
+    @endif
 @endif
