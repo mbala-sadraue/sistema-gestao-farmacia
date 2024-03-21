0ff1ed158f1ad18d4ddd8b0d6f4da88cff3655f9
Mbala Sadraque <orlandosadraquiorlandinho9@gmail.com>
2024-03-22 00:41:49 +0100

    criar controle de venda

---
+++ app/Http/Controllers/Panel/Vendedor/VendaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Panel\Vendedor;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class VendaController extends Controller
+{
+    //
+}
