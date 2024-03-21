e872c2008e5c2379383178e59f8421b8ed8021ce
Mbala Sadraque <orlandosadraquiorlandinho9@gmail.com>
2024-03-22 00:29:54 +0100

    criar model de cliente, venda  e itens vendido

---
+++ app/Models/Vendedor/Venda.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Vendedor;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Venda extends Model
+{
+    use HasFactory;
+}
