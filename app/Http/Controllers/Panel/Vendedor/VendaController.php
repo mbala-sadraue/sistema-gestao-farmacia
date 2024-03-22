e016a35d86dee5c99471c5681fd84575090ae398
Mbala Sadraque <orlandosadraquiorlandinho9@gmail.com>
2024-03-22 01:05:38 +0100

    criar o meto de cadastro de venda

--- app/Http/Controllers/Panel/Vendedor/VendaController.php
+++ app/Http/Controllers/Panel/Vendedor/VendaController.php
@@ -2,10 +2,44 @@
 
 namespace App\Http\Controllers\Panel\Vendedor;
 
+use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\Vendedor\Venda;
+use App\Models\Vendedor\ItensVendido;
 class VendaController extends Controller
 {
-    //
+
+
+
+
+    public function store_venda()
+    {
+
+        $venda = new Venda();
+        $insert_venda = $venda->create([
+
+            'data'              =>  $request->codProduto,
+            'user_id'           =>  $request->user_id,
+            'formaPg'           =>  $request->formaPg,
+            'quantCompra'       =>  $request->cliente_id,
+            'proddataHorato_id' =>  $request->dataHora,
+
+        ]);
+
+        $itensVendido = new ItensVendido();
+
+        foreach($request->produtos as $produto){
+            $total =  $produto['precoVenda'] * $produto['quantVendido']; 
+            $itensVendidos    = $itensVendido->create([
+                'itens_id'      =>  $produto['itens_id'],
+                'precoVenda'    =>  $produto['precoVenda'],
+                'itens_id'      =>  $insert_venda->id,
+                'precoTotal'    =>  $total,
+                'quantVendido'  =>  $produto['quantVendido'],
+           ]);
+        }
+
+    }
+        
 }
